2f83dc0a5881402913cf9594f5040e471db73e43
Behic Akgun <behica@gmail.com>
2017-05-03 20:43:37 +0000

    Checkpoint 27 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+5.times do
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData. random_sentence
+    )
+end
+users = User.all
+  
 15.times do
   Topic.create!(
     name:         RandomData.random_sentence,
@@ -9,9 +18,10 @@ topics = Topic.all
 50.times do
   
   Post.create!(
-    topic: topics.sample,
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
+    user:   users.sample,
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
@@ -23,7 +33,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'behica@gmail.com',
+  password: 'helloworld'
+  )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
